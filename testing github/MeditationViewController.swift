58a89e9eac5aee1db7daa01c38d153a3d205e321
Caitlin Keogh <ckeogh2006@yahoo.com>
2022-07-20 20:41:09 -0400

    Added two meditations

--- testing github/MeditationViewController.swift
+++ testing github/MeditationViewController.swift
@@ -5,17 +5,85 @@
 //  Created by Caitlin Keogh on 7/20/22.
 //
 
+import AVFoundation
 import UIKit
 
 class MeditationViewController: UIViewController {
 
+    @IBOutlet weak var selfLoveButton: UIButton!
+    @IBOutlet weak var angerReleaseButton: UIButton!
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func angerReleaseButtonPressed(_ sender: Any) {
+        if let player = player, player.isPlaying {
+            //stop playback
+        }
+        else {
+            let urlString = Bundle.main.path(forResource: "angerRelease", ofType: "mp3")
+            
+            //set up player and play
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else {
+                    return
+                }
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                player.play()
+        }
+            catch {
+                print("something went wrong")
+            }
+    }
+    }
+    
+    @IBAction func selfLoveButtonPressed(_ sender: Any) {
+        if let player = player, player.isPlaying {
+            //stop playback
+        }
+        else {
+            let urlString = Bundle.main.path(forResource: "selfLoveMeditation", ofType: "mp3")
+            
+            //set up player and play
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else {
+                    return
+                }
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                player.play()
+        }
+            catch {
+                print("something went wrong")
+            }
+    }
+        
+        
+        
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
