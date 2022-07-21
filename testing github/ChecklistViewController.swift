757c5d558e8d430e7ae333caff45460737f779f2
Caitlin Keogh <ckeogh2006@yahoo.com>
2022-07-21 14:11:23 -0400

    Made checklist

--- testing github/ChecklistViewController.swift
+++ testing github/ChecklistViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class ChecklistViewController: UIViewController {
+class ChecklistItem {
+    let title: String
+    var isChecked: Bool = false
 
+    init(title: String) {
+        self.title = title
+    }
+}
+
+
+class ChecklistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let items: [ChecklistItem] = [
+        "Journal",
+        "Take shower",
+        "Go on walk",
+        "Meditate",
+        "Listen to music",
+        "Go outside"
+    ].compactMap({
+        ChecklistItem(title: $0)
+    })
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        title = "Things to Do to Improve your Mental Health"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
@@ -25,5 +57,33 @@ class ChecklistViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    
+    //Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.isChecked ? .checkmark : .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = items[indexPath.row]
+        item.isChecked = !item.isChecked
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
 
 }
